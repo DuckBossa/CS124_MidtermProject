6f332be48e791c6b56adf53a8fbc962a0f6e0575
Vermille Saw <veeann@users.noreply.github.com>
2015-01-16 16:23:59 +0800

    Update Classes.java

--- Classes.java
+++ Classes.java
@@ -1,16 +1,12 @@
 class Memento {
-	
-	HashMap<String, Table> tables;
-	
+	private HashMap<String, Table> tables;
 	public Memento (HashMap<String, Table> t) {
 		setState (t);
 	}
-	
 	public HashMap<String, Table> getState () {
 		return tables;
 	}
-	
-	public void setState(HashMap<String, Table> t) {
+	private void setState(HashMap<String, Table> t) {
 		tables = t;
 	}
 }
