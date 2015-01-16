f3b6bb529b651afccf11ed3b5b6378abf23be334
J.Lo Serdanco <carlosponge@gmail.com>
2015-01-16 12:18:56 +0800

    updated db mediator

--- DB_Mediator.java
+++ DB_Mediator.java
@@ -3,13 +3,65 @@ import java.sql.*;
 public class DB_Mediator {
 	
 	Connection conn;
+	Statement statement;
 	
 	public DB_Mediator()
 	{
 		establishConnection("root", "", "MM");
 	}
 	
-	public Connection establishConnection(String username, String password, 
+	public ResultSet search(String query)
+	{
+		try{
+		return statement.executeQuery(query);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void executeUpdate(String query)
+	{
+		try{
+		statement.executeUpdate(query);
+		System.out.println("Update was successfully executed!");
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void clearSalesTable()
+	{
+		try{
+		statement.executeUpdate("DELETE FROM Sales;");
+		System.out.println("Successfully cleared Sales table!");
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void clearPackTable()
+	{
+		try{
+		statement.executeUpdate("DELETE FROM MMPack;");
+		System.out.println("Successfully cleared MMPack Table!");
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void clearAllTables()
+	{
+		clearSalesTable();
+		clearPackTable();
+	}
+	
+	
+	public void establishConnection(String username, String password, 
 	String db)
 	{	
 		
@@ -31,15 +83,14 @@ public class DB_Mediator {
 				conn = DriverManager.getConnection(URL);
 				
 			}
-			
+			statement = conn.createStatement();
 			System.out.println("Connection Established!");
-			return conn;
+			
 		} 
 		catch(Exception e)
 		{
 			System.out.println("Connection Failed!");
 			e.printStackTrace();
-			return null;
 		}
 		
 	}
